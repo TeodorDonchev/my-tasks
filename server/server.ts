43aeb9a740ff384f5cf2264a18b6ec73fb80456c
Teodor <teodorgdo@gmail.com>
2022-12-19 17:13:33 +0200

    added server
    added express js server

---
+++ server/server.ts
@@ -0,0 +1,23 @@
+
+import * as express from 'express';
+import {Application} from "express";
+import {getAllTasks} from "./get-tasks.route";
+
+const bodyParser = require('body-parser');
+
+const app: Application = express();
+
+const cors = require('cors');
+
+app.use(cors({origin: true}));
+
+app.use(bodyParser.json());
+
+app.route('/api/tasks').get(getAllTasks);
+
+const httpServer:any = app.listen(9001, () => {
+    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+});
+
+
+
